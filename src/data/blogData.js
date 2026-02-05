1b6d0eb732d9981a22f1cb7be2c11363be7e897f
kartikbindra <kartikbindra_23cs211@dtu.ac.in>
2026-02-05 17:09:15 +0530

    new blog added

---
+++ src/data/blogData.js
@@ -0,0 +1,21 @@
+import LeetCodeArchitecture from '../assets/leetcodeArchitecture2.png';
+import spiderman from '../assets/semantic-web-blog-asset.png';
+
+const blogData = [
+  {
+    id: 1,
+    title: "LeetCode EXPOSED : How It REALLY Works Behind the Scenes! 🔥",
+    description: "Dive into LeetCode's back-end architecture and code assessment processes that drive its speed and security.",
+    link: "https://kartikbindra.hashnode.dev/leetcode-exposed-how-it-really-works-behind-the-scenes",
+    image: LeetCodeArchitecture,
+  },
+  {
+    id: 1,
+    title: "What if I told you your 'next-gen AI Stack' is as old as Google?",
+    description: "Exposing RAG Context Engineering as the DOPPLEGANGER of Semantic Web propose by Sir Tim Berners-Lee.",
+    link: "https://open.substack.com/pub/kartikbindra/p/what-if-i-told-you-your-next-gen?r=3pux17&utm_campaign=post&utm_medium=web",
+    image: spiderman,
+  },
+];
+
+export default blogData;
